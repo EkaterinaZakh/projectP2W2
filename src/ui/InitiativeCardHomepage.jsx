463a11e769ee3c6d8d8a30fd29f4d5ff1dd25e15
sashanoviy <lamp.lamp.lamp@yandex.ru>
2024-03-01 13:27:39 +0300

    esho odna chush

--- src/ui/InitiativeCardHomepage.jsx
+++ src/ui/InitiativeCardHomepage.jsx
@@ -15,8 +15,11 @@ export default function InitiativeCardHomepage({ initiative, votingHandler }) {
                 </div>
                 <div className="card-body">
                   <h4>{initiative.description}</h4>
-                  <a className="btn btn-primary">Подробнее</a>
-                  <a className="btn btn-primary" onClick={() => votingHandler(initiative)}>Проголосовать</a>
+                  <h3>{initiative.district}</h3>
+                  <div className=" d-flex row">
+                    <a href={`/initiatives/${initiative.id}`} className="btn btn-primary">Подробнее</a>
+                    <a className="btn btn-success" onClick={() => votingHandler(initiative)}>Проголосовать</a>
+                  </div>
                   <p>
                     Голосов:
                     {initiative.voites}
