8a553808f42fbc01f9ade278f733bab5c0005cf2
EkaterinaZakh <zakharovaea95@yandex.ru>
2024-03-01 16:30:01 +0300

    final 2.0

---
+++ src/routes/notFoundPageRouter /notFoundPageRouter.js
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const notFoundPageRouter = express.Router();
+
+
+notFoundPageRouter.get('*', (req, res) => {
+    res.status(404).render('NotFoundPage');
+});
+
+
+
+export default notFoundPageRouter;
